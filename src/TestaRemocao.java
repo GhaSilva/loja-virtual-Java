6c97b406048a9de60d66db5e0c90a4ef4a5f4823
GhaSilva <ghabriel-silva@outlook.com>
2021-07-02 18:53:19 -0300

    Usando PreparedStatement na remocão de produto

--- src/TestaRemocao.java
+++ src/TestaRemocao.java
@@ -1,20 +1,19 @@
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class TestaRemocao {
 	public static void main(String[] args) throws SQLException {
 		ConnectionFactory connectionFactory = new ConnectionFactory();
 		Connection connection = connectionFactory.recuperarConexao();
 		
-		Statement stm = connection.createStatement();
-		
-		stm.execute("delete from produto where id > 2");
+		PreparedStatement stm = connection.prepareStatement("delete from produto where id > ?");
+		stm.setInt(1, 2);
+		stm.execute();
 		
 		int linhasModificadas = stm.getUpdateCount();
 		
 		System.out.println(linhasModificadas + " linhas foram modificadas");
-		
 		
 		connection.close();
 	}
