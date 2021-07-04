b91190547595143f58ce97c244334b7aa6b4ae31
GhaSilva <ghabriel-silva@outlook.com>
2021-07-04 15:50:28 -0300

    Adicionando varios itens ao banco de dados

--- src/TestaInsercaoComParametro.java
+++ src/TestaInsercaoComParametro.java
@@ -6,14 +6,23 @@ import java.sql.Statement;
 
 public class TestaInsercaoComParametro {
 	public static void main(String[] args) throws SQLException {
-		String nome = "Mouse";
-		String descricao = "Mouse sem fio";
+
 		
 		ConnectionFactory connectionFactory = new ConnectionFactory();
 		Connection connection = connectionFactory.recuperarConexao();
 		
 		PreparedStatement stm = connection.prepareStatement("insert into produto(nome, descricao) values(?, ?)", Statement.RETURN_GENERATED_KEYS);
 		
+		adicionarVariavel("Mouse", "Mouse sem fio", stm);
+		adicionarVariavel("SmartTV", "45 polegadas", stm);
+		adicionarVariavel("Rádio", "Rádio de bateria", stm);
+		
+		
+		connection.close();
+	}
+
+	private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm)
+			throws SQLException {
 		//parametros que vão substituir os "?"
 		stm.setString(1, nome);
 		stm.setString(2, descricao);
@@ -27,7 +36,8 @@ public class TestaInsercaoComParametro {
 			Integer id = rst.getInt(1);
 			System.out.println("O ID criado foi: " + id);
 		}
-		connection.close();
+		
 	}
+
 }
 
