7caf9086634fa6740dbc5be2bd349c02df32915e
GhaSilva <ghabriel-silva@outlook.com>
2021-07-05 19:11:03 -0300

    Começando pool de conexões

--- src/ConnectionFactory.java
+++ src/ConnectionFactory.java
@@ -1,11 +1,28 @@
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class ConnectionFactory {
 	
+	public DataSource dataSource;
+	
+	public ConnectionFactory() {
+		
+		//Criando pool de conexões para evitar sobrecarga do banco de dados
+		ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+		comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC");
+		comboPooledDataSource.setUser("root");
+		comboPooledDataSource.setPassword("200305Gh.");
+		
+		this.dataSource = comboPooledDataSource;
+	}
+	
+	
 	public Connection recuperarConexao() throws SQLException {
-		return DriverManager.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "200305Gh.");
+		return this.dataSource.getConnection();
 	}
 	
 }
